82c1eae5794a22c4a55760f6c658a7ac6f5f3f67
mohd faizan <mohdfaizan25122002@gmail.com>
2026-04-18 13:42:01 +0530

    Relative

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [sum,setSum]=useState(0);
+  function suminside(e){
+        let number =+e.target.value;
+          setSum(sum + number);
+
+        }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" onChange={suminside} />
+        <p>Sum:{sum}</p>
     </div>
   )
 }
