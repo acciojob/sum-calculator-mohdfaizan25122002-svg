aefb8838366c477196e317d991860415f225dc2d
mohd faizan <mohdfaizan25122002@gmail.com>
2026-04-18 14:36:51 +0530

    asd

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
         {/* Do not remove the main div */}
         <h1>Sum Calculator</h1>
         <input type="number" onChange={sumInside} />
-        <p>{sum}</p>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
